283192232629c78eceb671f9aac117c23d9fccb3
ThomasFuser <fuser.thomas@gmail.com>
2016-02-11 11:58:37 +0100

    modifica main. e db che si modifica da solo

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
 #include <QApplication>
-#include <iostream>
 
-#include "MODEL/Opera.h"
-#include "MODEL/Rivista.h"
-#include "MODEL/Libro.h"
 
 #include "MODEL/database.h"
-#include <VIEW/listaop.h>
-#include <VIEW/dettagli_rivista.h>
-#include <VIEW/dettagli_libro.h>
-//#include <VIEW/mainwindow.h>
-
-#include <CONTROLLER/c_listaop.h>
 #include <CONTROLLER/c_mainwindow.h>
 
 using namespace std;
@@ -19,15 +9,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv); 
-
-    DataBase db;
-    /*mainWindow* finestra=new mainWindow(&db);
-    C_mainWindow* controller=new C_mainWindow(&db,finestra); */
+    DataBase db; 
     mainWindow finestra(&db);
     C_mainWindow controller(&db,&finestra);
 
     finestra.show();
-
 
     return a.exec();
 }
